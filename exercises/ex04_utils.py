61df277867671bbf97992243dfc45e8a42f35469
Prasun <prasun1@unc.edu>
2023-10-05 20:44:03 -0400

    ex04-final

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -3,48 +3,35 @@
 __author__ = "730651920"
 
 
-def all (list_of_ints: list, number: int ) -> bool:
-    list_idx: int = 1
-    if list_of_ints[list_idx] == number:
-        while list_idx < len(list_of_ints):
-          if list_of_ints[list_idx-1] == list_of_ints[list_idx]:
-            list_idx += 1
-            return True
-          else:
-            list_idx = len(list_of_ints) 
-            return False   
-    else:
-       return False
-        
-def max (list_of_ints: list) -> int:
+def all(list_of_ints: list[int], number: int) -> bool:
+    """Checks if all values in list are identical."""
     if len(list_of_ints) == 0:
-        raise ValueError("max() arg is an empty List")
-    list_idx: int = 1
+        return False
+    list_idx: int = 0
+    while list_idx < len(list_of_ints):
+        if list_of_ints[list_idx] == number:
+            list_idx += 1
+        else:
+            return False  
+    return True
+
+
+def max(list_of_ints: list[int]) -> int:
+    """Finds the max in a list of intergers."""
+    if len(list_of_ints) == 0:
+        raise ValueError
+    list_idx: int = 0
     Max_int: int = list_of_ints[0]
     while list_idx < len(list_of_ints):
-        if list_of_ints[list_idx-1] <= list_of_ints[list_idx]:
+        if list_of_ints[list_idx] > Max_int:
             Max_int = list_of_ints[list_idx]
         list_idx += 1
     return Max_int
-   
-def lol_equal(list1: list, list2: list) -> bool:
-   if list1 == list2:
-      return True
-   else:
-      return False
-   
-print(lol_equal([1, 0, 1], [1, 0, 1, 0]))
 
 
-
-
-    
-   
-    
-
-
-
-
-    
-
-
+def is_equal(list1: list[int], list2: list[int]) -> bool:
+    """Checks if values in one list equal another in different list."""
+    if list1 == list2:
+        return True
+    else:
+        return False
